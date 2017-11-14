3b57e9aba52b0062b7a32e9e61f38f6c144aa98e
mxz96102 <mxz96102@qq.com>
2017-11-14 21:41:11 +0800

    feat: new start with less ambition

--- src/global.js
+++
@@ -1,9 +0,0 @@
-/**
- * Global Tool Inject and Const Define
- */
-
-function GlobalDefine() {
-    global.argv = require('./command').commandPrepare();
-}
-
-exports = module.exports = GlobalDefine;
